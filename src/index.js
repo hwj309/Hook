42871d5487054fc9a751a33f1b6c1e315f93aaff
hwj309 <99112387+hwj309@users.noreply.github.com>
2024-07-24 09:37:55 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './sample/useState/Counter';
+import Info from './sample/useEffect/Info';
+import ContextSample from './sample/useContext/ContextSample';
+import Backing from './sample/useReducer/Baking';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter />
+    <p>=========================================================================</p>
+    <Info />
+    <p>=========================================================================</p>
+    <ContextSample />
+    <p>=========================================================================</p>
+    <Backing />
+    
   </React.StrictMode>
 );
+//root.render(<Counter />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
